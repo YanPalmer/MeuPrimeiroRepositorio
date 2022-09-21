68d60168fa851be891cc709fa7ec3b1239bc112d
YanPalmer <86450167+YanPalmer@users.noreply.github.com>
2022-09-20 21:54:51 -0300

    Terminei o visual
    Os textos já estão postos, agora me falta criar um banco de dados para armazenar os alimentos que serão comprados pelo consumidor.

--- MeusProjetosGitHub/p1_NotaFiscal/nFscript.js
+++ MeusProjetosGitHub/p1_NotaFiscal/nFscript.js
@@ -14,5 +14,18 @@ horario = new Date()
 horas = horario.getHours()
 minutos = horario.getMinutes()
 segundos = horario.getSeconds()
+
+if (horas < 10) {horas = "0" + horas;}
+if (segundos < 10) {segundos = "0" + segundos;}
+
+
 document.getElementById("data").textContent += " "+ horas +":"+ minutos +":"+ segundos
 document.getElementById("data").textContent += " VENDA CAIXA N.1234" // Depois criar um programa que verifique o tipo de caixa que o vendedor está utilizando
+
+function sum(x, y) {
+    a = 4+2
+    return a;
+}
+console.log(sum(a))
+
+this.print
